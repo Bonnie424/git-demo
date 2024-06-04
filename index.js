fc2e5e24035a9a26fb25ecff8ab129eaabf2d3fb
AlanYu <squid78313@gmail.com>
2024-06-04 17:37:03 +0800

    將.json轉成js，再引入index.html，並透過index.js來將wallet list渲染在畫面

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-let name = "bonnie"
-console.log(`name = ${name}`)
+const walletInfoSection = document.querySelector('#wallet-info-section')
 
+let walletListHtml = ``
+for (let i = 0; i < walletList.length; i++) {
+  walletListHtml += `
+    <div class="wallet-info">
+      <div>${i + 1}號選手</div>
+      <div class="wallet-info-brand"><span>品牌: </span>${walletList[i].brand}</div>
+      <div class="wallet-info-model"><span>型號: </span>${walletList[i].model}</div>
+      <a class="wallet-info-url" href="${walletList[i].url}" target="_blank">前往${walletList[i].model}</a>
+    </div>
+    <hr>
+  `
+}
+
+walletInfoSection.innerHTML = walletListHtml
---
+++ wallet-list.js
@@ -0,0 +1,32 @@
+const walletList = [
+  {
+    "id": 1,
+    "brand": "bellroy",
+    "model": "coin wallet",
+    "url": "https://zh-tw.bellroy.com/products/coin-wallet?color=black&material=leather_rfid#slide-0"
+  },
+  {
+    "id": 2,
+    "brand": "bellroy",
+    "model": "note sleeve",
+    "url": "https://zh-tw.bellroy.com/products/note-sleeve-wallet?color=black&material=leather_rfid#slide-0"
+  },
+  {
+    "id": 3,
+    "brand": "bellroy",
+    "model": "zip wallet",
+    "url": "https://zh-tw.bellroy.com/products/zip-wallet?color=black&material=leather_rfid"
+  },
+  {
+    "id": 4,
+    "brand": "porter",
+    "model": "centric",
+    "url": "https://www.ll-porter.com/tw/product-detail/11713-01701/"
+  },
+  {
+    "id": 5,
+    "brand": "porter",
+    "model": "bend",
+    "url": "https://www.ll-porter.com/tw/product-detail/11344-00601"
+  }
+]
