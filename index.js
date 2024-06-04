381e9f4c81bcce5f93ba68487c00c60ad9603049
AlanYu <squid78313@gmail.com>
2024-06-04 17:46:39 +0800

    利用axios請求wallet-list.json，在透過index.js將其渲染在畫面

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const walletInfoSection = document.querySelector('#wallet-info-section')
 let name = "bonnie"
-console.log(`name = ${name}`)
+
+axios.get('wallet-list.json').then(response => {
+  const walletList = response.data
+  let walletListHtml = ''
+  for (let i = 0; i < walletList.length; i++) {
+    walletListHtml += `
+      <div class="wallet-info">
+        <div>${i + 1}號選手</div>
+        <div class="wallet-info-brand"><span>品牌: </span>${walletList[i].brand}</div>
+        <div class="wallet-info-model"><span>型號: </span>${walletList[i].model}</div>
+        <a class="wallet-info-url" href="${walletList[i].url}" target="_blank">前往${walletList[i].model}</a>
+      </div>
+      <hr>
+    `
+  }
+  walletInfoSection.innerHTML = walletListHtml
+}).catch(error => {
+  console.log(error)
+})
+
 
